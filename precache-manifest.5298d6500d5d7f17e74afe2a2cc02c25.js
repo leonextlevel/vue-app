f04df719062588af929ecd7194aed1fad8b5175e
llbueno <leandro.bueno@fatec.sp.gov.br>
2020-01-30 10:16:26 -0300

    Configurações para github pages

---
+++ precache-manifest.5298d6500d5d7f17e74afe2a2cc02c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dde64737728fc1dda9",
+    "url": "/css/chunk-vendors.c00f6baf.css"
+  },
+  {
+    "revision": "c1673af6d770f6b32a2e47efbf023174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f1cf3e681cb1cb421a",
+    "url": "/js/app.a6833322.js"
+  },
+  {
+    "revision": "95dde64737728fc1dda9",
+    "url": "/js/chunk-vendors.238fee62.js"
+  },
+  {
+    "revision": "ef24d95719f240c7d2e7fc141176e936",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
