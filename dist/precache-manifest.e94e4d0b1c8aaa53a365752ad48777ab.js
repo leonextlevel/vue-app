34ca58b24a15ef0a7689f0346deb4b6da71125fb
LLBueno <leandro.bueno@fatec.sp.gov.br>
2020-02-01 12:39:25 -0300

    configuração para deploy no Heroku

---
+++ dist/precache-manifest.e94e4d0b1c8aaa53a365752ad48777ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6636b8eb2b330c420e29",
+    "url": "/css/chunk-vendors.c00f6baf.css"
+  },
+  {
+    "revision": "2f7711c0d8845695ec8fac3413688e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e958a11380b235c2ba",
+    "url": "/js/app.71aff918.js"
+  },
+  {
+    "revision": "6636b8eb2b330c420e29",
+    "url": "/js/chunk-vendors.e12911c3.js"
+  },
+  {
+    "revision": "ef24d95719f240c7d2e7fc141176e936",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
